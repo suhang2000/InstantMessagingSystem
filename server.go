91fe1086ce414ec963b2dc101dab9c82748d9064
LI Suhang <lisuhang2000@gmail.com>
2022-12-12 02:35:18 +0900

    V0.2 User online, broadcast

--- server.go
+++ server.go
@@ -3,15 +3,26 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+	// online users
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+	// broadcast channel
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
-	return &Server{ip, port}
+	return &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
 }
 
 func (this *Server) Start() {
@@ -28,6 +39,8 @@ func (this *Server) Start() {
 		}
 	}(listener)
 
+	go this.ListenMessager()
+
 	for {
 		// accept
 		conn, err := listener.Accept()
@@ -41,5 +54,31 @@ func (this *Server) Start() {
 }
 
 func (this *Server) Handler(conn net.Conn) {
-	fmt.Println("build connection successfully")
+	fmt.Println("Server Handler: build connection successfully")
+	user := NewUser(conn)
+	// add user to onlineMap
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+	// broadcast message
+	this.BroadCast(user, "online")
+	select {}
+
+}
+
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
+func (this *Server) ListenMessager() {
+	for {
+		msg := <-this.Message
+		// send msg to all online users
+		this.mapLock.Lock()
+		for _, user := range this.OnlineMap {
+			user.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
 }
---
+++ user.go
@@ -0,0 +1,34 @@
+package main
+
+import "net"
+
+type User struct {
+	Name string
+	Addr string
+	C    chan string
+	conn net.Conn
+}
+
+// NewUser : create user
+func NewUser(conn net.Conn) *User {
+	userAddr := conn.RemoteAddr().String()
+	user := &User{
+		Name: userAddr,
+		Addr: userAddr,
+		C:    make(chan string),
+		conn: conn,
+	}
+
+	// start listening
+	go user.listenMessage()
+
+	return user
+}
+
+// listen message of a user
+func (this *User) listenMessage() {
+	for {
+		msg := <-this.C
+		this.conn.Write([]byte(msg + "\n"))
+	}
+}
